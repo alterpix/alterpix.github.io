207ebb8d5ebff3ac1c3ff4b25a75b042bcb7d4f1
alterpix <134699367+alterpix@users.noreply.github.com>
2024-08-31 13:48:43 +0700

    Add files via upload

---
+++ assets/script.js
@@ -0,0 +1,12 @@
+var typed = new Typed("#element", {
+    strings: [
+        "web Developer",
+        "Web Designer",
+        "Freelancer",
+        "Graphic Designer",
+        "UI/UX Designer",
+    ],
+    typeSpeed: 100,
+    loop: true,
+    shuffle: true,
+})
